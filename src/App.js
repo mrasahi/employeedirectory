8083fafea214003fe24d921aaa7410f2382c6f46
Mak <makotorasahi@gmail.com>
2020-09-18 18:42:00 -0700

    picture thumbnail working

--- src/App.js
+++ src/App.js
@@ -8,47 +8,57 @@ const App = () => {
   // Employee State
   const [employeeState, setEmployeeState] = useState({
     employees: [],
-    columns: [{
-      Header: 'Name',
-      accessor: 'name' // String-based value accessors!
-    },
-    {
-      Header: 'Email',
-      accessor: 'email',
-      // Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
-    },
-    {
-      Header: 'Phone',
-      accessor: 'phone' // String-based value accessors!
-    },
-    {
-      Header: 'Address',
-      accessor: 'address' // String-based value accessors!
-    }]
-
+    columns: [
+      {
+        Header: 'Photo',
+        accessor: 'picture',
+        Cell: props => <span className='number'>{props.value}</span>,
+        filterable: false,
+        sortable: false,
+        minWidth: 15
+      },
+      {
+        Header: 'Name',
+        accessor: 'name',
+        Cell: props => <span className='number'>{props.value}</span>,
+        filterable: true
+      },
+      {
+        Header: 'Email',
+        accessor: 'email',
+        Cell: props => <span className='number'>{props.value}</span>,
+        filterable: true
+      },
+      {
+        Header: 'Phone',
+        accessor: 'phone',
+        Cell: props => <span className='number'>{props.value}</span>,
+        filterable: true
+      },
+      {
+        Header: 'Address',
+        accessor: 'address',
+        Cell: props => <span className='number'>{props.value}</span>,
+        filterable: true,
+      }
+    ]
   })
 
-  employeeState.button = event => {
-    console.log(employeeState.employees)
-  }
+  // // For Debugging
+  // employeeState.button = event => {
+  //   console.log(employeeState.employees)
+  // }
 
 
   useEffect(() => {
     axios.get('https://randomuser.me/api?results=20')
       .then(({ data }) => {
         console.log(data.results)
-        // let employees = data.results.map(employee => ({
-        //   name: `${employee.name.first} ${employee.name.last}`,
-        //   email: employee.email,
-        //   phone: employee.phone,
-        //   address: `${employee.location.city}, ${employee.location.country}`
-        // }))
-        // setEmployeeState({ employees })
-        setEmployeeState({ employees: data.results.map(individual => ({
-          name: `${individual.name.title} ${individual.name.first} ${individual.name.last}`,
+        setEmployeeState({ ...employeeState, employees: data.results.map(individual => ({
+          name: `${individual.name.first} ${individual.name.last}`,
           email: individual.email,
           phone: individual.phone,
-          // picture: individual.picture.thumbnail,
+          picture: <img src={individual.picture.thumbnail} alt={individual.name.first} />,
           address: `${individual.location.city}, ${individual.location.country}`
         }))
       })
@@ -58,10 +68,13 @@ const App = () => {
   return (
     <>
       <h1>Employee Directory</h1>
-      <button onClick={employeeState.button}>See States</button>
+      {/* <button onClick={employeeState.button}>See States</button> */}
       <ReactTable
         data={employeeState.employees}
         columns={employeeState.columns}
+        resolveData={data => data.map(row => row)}
+        pageSizeOptions={[1, 5, 10, 15, 20]}
+        filterable={true}
       />
     </>
   )
